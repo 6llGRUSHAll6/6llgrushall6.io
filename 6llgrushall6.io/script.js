3b7620422e5406a4ed44fdaa8f436737b8757991
6llgrushall6 <sherg6431@gmail.com>
2025-03-01 21:18:10 +0700

    Add files via upload

---
+++ 6llgrushall6.io/script.js
@@ -0,0 +1,13 @@
+// Функция для перехода на главную страницу
+function goToHomePage() {
+    window.location.href = 'index.html'; // Перенаправление на главную страницу (index.html)
+}
+
+function goTo2page() {
+    window.location.href = 'page2.html';
+}
+
+function Obnovi() {
+    window.location.href = 'logs.html';
+}
+
